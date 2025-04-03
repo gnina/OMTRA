35798b214fa28f2859cc6450f4bff21f16ca9b64
katztyler01 <tjkatz@andrew.cmu.edu>
2025-04-02 21:39:33 -0400

    feat: add radius and knn fns for inter ntype edges to edge_builders

--- omtra/data/graph/edge_builders.py
+++ omtra/data/graph/edge_builders.py
@@ -16,3 +16,12 @@ def complete_graph(node_positions):
                              loop=False, 
                              max_num_neighbors=n_nodes+2)
     return edge_idxs
+
+
+def hetero_radius(src_pos, dst_pos, src_batch_idx, dst_batch_idx, r, max_num_neighbors=1000):
+    edge_idxs = tc.radius(x=dst_pos, y=src_pos, batch_x=dst_batch_idx, batch_y=src_batch_idx, r=r, max_num_neighbors=max_num_neighbors)
+    return edge_idxs
+
+def hetero_knn(src_pos, dst_pos, src_batch_idx, dst_batch_idx, k):
+    edge_idxs = tc.knn(x=dst_pos, y=src_pos, batch_x=dst_batch_idx, batch_y=src_batch_idx, k=k)
+    return edge_idxs
