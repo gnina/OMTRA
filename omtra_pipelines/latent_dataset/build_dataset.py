831b6562e3c6c1ced6a5ea553a8a1e32da520185
Ramith Hettiarachchi <ramith@cmu.edu>
2025-06-15 23:06:08 -0400

    save as a dataset zarr instead

--- omtra_pipelines/latent_dataset/build_dataset.py
+++ omtra_pipelines/latent_dataset/build_dataset.py
@@ -1,9 +1,16 @@
+import zarr
+from tqdm import tqdm
+from pathlib import Path
+
 import torch
 import numpy as np
+
 from rdkit.Chem import rdMolAlign
 from omtra.load.quick import datamodule_from_config
+from omtra.utils.graph import build_lookup_table
 import omtra.load.quick as quick_load
 
+from zarr_helpers import init_zarr_store_latents 
 from utils import get_gt_as_rdkit_ligand, find_rigid_alignment
 
 cfg = quick_load.load_cfg(overrides=['task_group=no_protein']) # you can provide overrides to the default config via command-line syntax here
@@ -11,17 +18,48 @@ datamodule = datamodule_from_config(cfg)
 train_dataset = datamodule.load_dataset("val")
 pharmit_dataset = train_dataset.datasets['pharmit']
 
-batch_size = 128
-num_total_samples = 512 # len(pharmit_dataset) changed for testing
+batch_size = 32
+num_total_samples = 128 #len(pharmit_dataset) # 512changed for testing
 ckpt_path = '/home/ruh/Research/OMTRA/models/batch_295000.ckpt'
 
+output_zarr_path = Path("confidence_dataset.zarr")
+n_zarr_chunks = 2
 
 model = quick_load.omtra_from_checkpoint(ckpt_path).cuda().eval()
+
+# Pre-compute latent dimensions
+print("Pre-computing latent dimensions")
+atom_counts_per_sample = []
+for i in tqdm(range(num_total_samples), desc="Sizing dataset"):
+    g = pharmit_dataset[('ligand_conformer', i)]
+    atom_counts_per_sample.append(g.num_nodes('lig'))
+
+total_atoms = sum(atom_counts_per_sample)
+
+
+dataset_spec = {
+    'n_mols' : num_total_samples,
+    'n_atoms': total_atoms,
+    'scalar_dim': model.vector_field.n_hidden_scalars,
+    'vector_dim': model.vector_field.n_vec_channels
+}
+
+graph_lookup_table = build_lookup_table(np.array(atom_counts_per_sample))
+
+_, root = init_zarr_store_latents(
+    store_path=output_zarr_path, 
+    totals=dataset_spec,
+    n_chunks=n_zarr_chunks
+)
+
+# Pre-populate the lookup table, as it's fully computed now
+root['metadata/graph_lookup'][:] = graph_lookup_table
+
 
 records = []
 delta = [] # evaluating how different kabsch_rmsd is to rdkit_rmsd
 
-for i in range(0, num_total_samples, batch_size):    
+for i in tqdm(range(0, num_total_samples, batch_size), desc="Processing Batches"): 
     current_batch_idx = range(i, min(i + batch_size, num_total_samples))
     
     g_list = [] 
@@ -36,20 +74,21 @@ for i in range(0, num_total_samples, batch_size):
         unconditional_n_atoms_dist='pharmit',
         n_timesteps=200,
         visualize=True,
-        extract_latents_for_confidence = True 
+        extract_latents_for_confidence=True 
     )
 
-    print(".", end="")
+    for j, sample_idx in enumerate(current_batch_idx):
+        # Pos of atom in Zarr arrays
+        atom_start, atom_end = graph_lookup_table[sample_idx]
 
-    for idx in range(0, batch_size):
-        pred_coords = sampled_systems[idx].g.nodes["lig"].data["x_1"].cpu()
-        gt_coords   = g_list[idx].nodes['lig'].data['x_1_true'].cpu()
+        pred_coords = sampled_systems[j].g.nodes["lig"].data["x_1"].cpu()
+        gt_coords   = g_list[j].nodes['lig'].data['x_1_true'].cpu()
     
-        pred_coords_rdkit = sampled_systems[idx].get_rdkit_ligand()
-        gt_coords_rdkit   = get_gt_as_rdkit_ligand(g_list[idx])
+        pred_coords_rdkit = sampled_systems[j].get_rdkit_ligand()
+        gt_coords_rdkit   = get_gt_as_rdkit_ligand(g_list[j])
 
         pred_aligned = find_rigid_alignment(pred_coords, gt_coords)
-        kabsch_rmsd = torch.sqrt(((pred_aligned - gt_coords)**2).sum(axis=1).mean())
+        kabsch_rmsd = torch.sqrt(((pred_aligned - gt_coords)**2).sum(axis=1).mean()).item()
     
         rdkit_rmsd = rdMolAlign.GetBestRMS(pred_coords_rdkit, gt_coords_rdkit)
     
@@ -57,23 +96,30 @@ for i in range(0, num_total_samples, batch_size):
         delta.append(kabsch_rmsd - rdkit_rmsd)
         
         latents = {
-            "node_scalar_features" : sampled_systems[idx].g.nodes["lig"].data["node_scalar_features"],
-            "node_vec_features" : sampled_systems[idx].g.nodes["lig"].data["node_vec_features"],
-            "node_positions" : sampled_systems[idx].g.nodes["lig"].data["node_positions"],
+            "node_scalar_features" : sampled_systems[j].g.nodes["lig"].data["node_scalar_features"].cpu().numpy(),
+            "node_vec_features" : sampled_systems[j].g.nodes["lig"].data["node_vec_features"].cpu().numpy(),
+            "node_positions" : sampled_systems[j].g.nodes["lig"].data["node_positions"].cpu().numpy(),
         }
+
+        # write to zarr
+        root['latents/node_scalar_features'][atom_start:atom_end] = latents["node_scalar_features"]
+        root['latents/node_vec_features'][atom_start:atom_end] = latents["node_vec_features"]
+        root['latents/node_positions'][atom_start:atom_end] = latents["node_positions"]
         
-        records.append({
-            "latents"    : latents,
-            "coords_gt"  : gt_coords,
-            "coords_pred": pred_coords,
-            "GetBestRMS" : rdkit_rmsd,
-            "Kabsch_RMSD" : kabsch_rmsd
-        })
+        root['coordinates/coords_gt'][atom_start:atom_end] = gt_coords.numpy()
+        root['coordinates/coords_pred'][atom_start:atom_end] = pred_coords.numpy()
+        
+        root['metrics/rdkit_rmsd'][sample_idx] = rdkit_rmsd
+        root['metrics/kabsch_rmsd'][sample_idx] = kabsch_rmsd
+
+        # # print shapes
+        # print("example", i + j)
+        # print("latents shape (node_scalar_features) ",latents['node_scalar_features'].shape)
+        # print("latents shape (node_vec_features) ",latents['node_vec_features'].shape)
+        # print("latents shape (node_positions) ",latents['node_positions'].shape)
+        # print("gt_coords shape ",gt_coords.shape)
+        # print("pred_coords ", pred_coords.shape)
 
 # print stats
 delta = np.array(delta)
-print(f"\nMean delta (Kabsch - GetBestRMS): {delta.mean()}")    
-
-out_file = "confidence_dataset.pt"
-torch.save(records, out_file)
-print(f"✓ Saved {len(records)} examples to {out_file}")
+print(f"\nMean delta (Kabsch - GetBestRMS): {delta.mean()}")
