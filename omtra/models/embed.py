5dacdd35ab1beefe6be5612a160ee7442943e79c
Ian Dunn <ian@iandunn.io>
2025-09-25 14:00:18 -0400

    embed funcs

---
+++ omtra/models/embed.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import math
+
+class TimestepEmbedder(nn.Module):
+    """Embeds scalar timesteps into vector representations."""
+
+    def __init__(self, hidden_dim, frequency_embedding_dim=256):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(frequency_embedding_dim, hidden_dim, bias=True),
+            nn.SiLU(),
+            nn.Linear(hidden_dim, hidden_dim, bias=True),
+        )
+        self.frequency_embedding_dim = frequency_embedding_dim
+
+    @staticmethod
+    def timestep_embedding(t, dim, max_period=10000):
+        half = dim // 2
+        freqs = torch.exp(
+            -math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half
+        ).to(device=t.device)
+        args = t[:, None].float() * freqs[None]
+        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+        if dim % 2:
+            embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+        return embedding
+
+    def forward(self, t):
+        t_freq = self.timestep_embedding(t, self.frequency_embedding_dim)
+        t_emb = self.mlp(t_freq)
+        return t_emb
+
+
+def get_pos_embedding(indices, emb_dim, max_len=2048):
+    """Creates sine / cosine poDiTional embeddings from a prespecified indices.
+
+    Args:
+        indices: offsets of size [..., num_tokens] of type integer
+        emb_dim: embedding dimension
+        max_len: maximum length
+
+    Returns:
+        poDiTional embedding of shape [..., num_tokens, emb_dim]
+    """
+    K = torch.arange(emb_dim // 2, device=indices.device)
+    pos_embedding_sin = torch.sin(
+        indices[..., None] * math.pi / (max_len ** (2 * K[None] / emb_dim))
+    ).to(indices.device)
+    pos_embedding_cos = torch.cos(
+        indices[..., None] * math.pi / (max_len ** (2 * K[None] / emb_dim))
+    ).to(indices.device)
+    pos_embedding = torch.cat([pos_embedding_sin, pos_embedding_cos], axis=-1)
+    return pos_embedding
