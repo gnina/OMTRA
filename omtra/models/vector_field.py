04326756e7344a322826b64866f1c43f3bde1f65
Ian Dunn <ian@iandunn.io>
2025-10-05 13:58:31 -0400

    remove zero gates

--- omtra/models/vector_field.py
+++ omtra/models/vector_field.py
@@ -1303,7 +1303,8 @@ class NodePositionUpdate(nn.Module):
                            adaln_params['s_shift'][batch_idxs], 
                            adaln_params['s_scale'][batch_idxs]
                            )
-        vectors = vectors * adaln_params['v_scale'][batch_idxs].unsqueeze(-1)
+        v_gate = adaln_params['v_gate'][batch_idxs].unsqueeze(-1) + 1
+        vectors = vectors * v_gate
 
         _, vector_updates = self.gvps((scalars, vectors))
         return positions + vector_updates.squeeze(1)
